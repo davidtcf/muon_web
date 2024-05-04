b51ddae4b784fd47d4f6e2e6031f4c01cbf95aa7
Chenfei Tang <tangchenfei2758@163.com>
2024-05-04 19:22:33 +0000

    Change

--- LuaSnippers/tex/physics.lua
+++ LuaSnippers/tex/physics.lua
@@ -6,8 +6,12 @@ s("muz",{t("\\mu_0")}),
 
 s("uv", fmt([[\hat{<>_{<>}} ]],{i(1),i(2)},{delimiters="<>"})),
 
-s("s", fmt([[_{<>}]],{i(1)},{delimiters="<>"}))
+s("s", fmt([[_{<>}]],{i(1)},{delimiters="<>"})),
 
+--Bra-Ket Notation--
+s("ket", fmt([[$\ket{<>}$]],{i(1),},{delimiters="<>"})),
+
+s("bra", fmt([[$\bra{<>}$]],{i(1),},{delimiters="<>"}))
 
 
 
